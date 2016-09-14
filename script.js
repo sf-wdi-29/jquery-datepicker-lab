b5bc80fa2fae4e67c0c05d7aa3bcfdbc0d22ff30
virginiachae <chae.virginia@gmail.com>
2016-09-13 23:07:49 -0700

    virginias attempt at step-2

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ function daysLeft() {
 // 7. daysLeft will need some local variables. Let's define the first one as `a`
 // You need to call datepicker's 'getDate' method and then you have to call the Date() object's .getTime(); method on the whole thing:
 
-var a = 
+var a = today.getTime()
+};
 
 // 8. Outside of the daysLeft() function, we need to call daysLeft() using jQuery's .on('change', ____) event handler.
 // Implement the function so it logs the value of `a` every time you click a date on the calendar.
